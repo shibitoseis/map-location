1594ecb1293cd1d6747f65842a1916386a9b725d
shibitoseis <33573755+shibitoseis@users.noreply.github.com>
2018-12-26 23:16:33 +0100

    Update reservation.js

--- js/reservation.js
+++ js/reservation.js
@@ -46,8 +46,8 @@ var reservation = {
 		sessionStorage.statusReservation = true;
 		sessionStorage.nomStationReserver = currentStation.nom;
 		sessionStorage.dureeReservation = reservationValidity * 60;
-		localStorage.setItem('prenom','#prenom');
-		localStorage.setItem('nom','#nom');
+		localStorage.setItem('prenom',$('#prenom'));
+		localStorage.setItem('nom',$('#nom'));
 		localStorage.getItem('prenom');
 		localStorage.getItem('nom');
 		localStorage.setItem('nom', document.querySelector("input").value);
